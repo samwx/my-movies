4be98ec36113981ad4f5e3e71e7bff1acaa2d31e
samwx <samuelmartins.sw@gmail.com>
2021-10-28 12:52:38 -0300

    Fix hooks deps

--- src/views/MovieDetail.js
+++ src/views/MovieDetail.js
@@ -6,14 +6,15 @@ export const MovieDetail = () => {
     const { id } = useParams();
     const [movie, setMovie] = useState({});
 
-    const fetchMovie = async () => {
-        const { data } = await MoviesService.getMovieById(id);
-        setMovie(data);
-    }
 
     useEffect(() => {
+        const fetchMovie = async () => {
+            const { data } = await MoviesService.getMovieById(id);
+            setMovie(data);
+        }
+
         fetchMovie();
-    }, []);
+    }, [id]);
 
     return (
         <>
